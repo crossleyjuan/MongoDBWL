fd5e6eed37a8852601d9b9dc859fd87ac7a73082
Juan Pablo Crossley <Cross>
2024-05-09 13:19:06 +0200

    Removed file

--- run.sh
+++
@@ -1,6 +0,0 @@
-#java -jar loadTestErmetic_20231222.jar -u "mongodb+srv://juancrossley:JpnNqSFKnD7sokWS@ermetic.mpxmc.mongodb.net/?retryWrites=true" -t 40 -d 110000 -c 20000 -i "contexts_to_force.txt"
-export URI='mongodb+srv://juancrossley:JpnNqSFKnD7sokWS@ermeticb.mpxmc.mongodb.net/?retryWrites=true&w=majority'
-java -jar loadTestErmetic_20231222.jar -u "${URI}" -t 40 -d 110000 -c 20000  -i "contexts_to_force.txt" -p true -f false -q false -a false
-
-
-
